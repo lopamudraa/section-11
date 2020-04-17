1e55f4912a2aae52a44351be8d8f8d6e0878b98e
Lopamudra Mishra <lopamudra5346@gmail.com>
2020-04-18 00:51:50 +0530

    201 video changes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -16,13 +16,21 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink 
+                            to="/" 
+                            exact
+                            activeClassName="my-active"
+                            activeStyle={{ //inline styleing
+                                color: '#fa923f',
+                                textDecoration: 'underline'
+                            }}
+                            >Home</NavLink></li>
+                            <li><NavLink to={{
                                // pathname: this.props.match.url + '/new-post', //to create Relative path
                                pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
